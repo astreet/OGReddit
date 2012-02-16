3a954ec7c27efb025389dc7cfdc35f2a55df87e7
Andy Street <andrews@fb.com>
2012-02-16 00:41:31 -0800

    1

--- extension/background.js
+++ extension/background.js
@@ -1,24 +1,131 @@
 $(function() {
-  function load_fbjs() {
-    (function(d){
-      var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-      js = d.createElement('script'); js.id = id; js.async = true;
-      js.src = "//connect.facebook.net/en_US/all.js";
-      d.getElementsByTagName('head')[0].appendChild(js);
-    }(document));
+  var connected = false;
+  var accessToken = null;
+
+  function connect() {
+    connected = true;
   }
 
-  $.append(
-    '<div class="fbPopupNib">' +
-      '<div class="fbPopup">' +
-        '<div class="fb-login-button">Login with Facebook</div>' +
-      '</div>' +
+  function disconnect() {
+    connected = false;
+  }
+
+  function setStatusOnResponse(response) {
+    switch (response.status) {
+      case 'connected':
+        accessToken = response.authResponse.accessToken;
+        connect();
+        break;
+      default:
+        disconnect();
+    }
+  }
+
+  var MAX_RETRIES = 3;
+  function retryActionWithParams(action, params, retry_count) {
+    if (retry_count === MAX_RETRIES) {
+      return;
+    }
+
+    var url = 'http://ogreddit.herokuapp.com/?' + $.param(params, true) + '&type=fbreddit:post';
+    console.log('Try ' + (retry_count + 1) + ': ' + url);
+    $.post(
+      'https://graph.facebook.com/me/fbreddit:' + action,
+      {post: url, access_token: accessToken}
+    ).error(function(xhr, text) {
+      if (text == 'parsererror') {
+        return;
+      }
+
+      console.error('Fail: ' + text);
+      setTimeout(function() {
+        retryActionWithParams(action, params, retry_count + 1);
+      }, 1000);
+    });
+  }
+
+  function doActionWithParams(action, params) {
+    retryActionWithParams(action, params, 0);
+  }
+
+  function upvote(button) {
+    if (!connected) {
+      return;
+    }
+
+    var entry = button.parents('.thing');
+    var titleElem =
+      entry
+        .children('.entry').first()
+        .children('p.title').first()
+        .children('a.title').first();
+
+    var title = titleElem.text();
+    var link = titleElem.attr('href');
+    if (link && link[0] === '/') {
+      link = 'http://reddit.com' + link;
+    }
+    var author =
+      entry
+        .children('.entry').first()
+        .children('.tagline').first()
+        .children('.author').first()
+        .text();
+
+    var image = 'http://www.redditstatic.com/over18.png';
+    var thumb = entry.children('.thumbnail');
+    if (thumb.length > 0) {
+      image = thumb.children('img').attr('src');
+    }
+
+    doActionWithParams('upvote', {
+      title: title,
+      link: link,
+      image: image,
+      author: author,
+      //type: 'fbreddit:post'
+    });
+  }
+
+  function unupvote(button) {
+    console.log('unupvote');
+  }
+
+  $('body').append(
+    '<div id="fb-root" />' +
+    '<img class="fbPopupNib" />' +
+    '<div class="fbPopup">' +
+      '<div class="fb-add-to-timeline" data-show-faces="true" data-mode="button"></div>' +
     '</div>'
   );
 
-  var popup = $('fbPopup');
-  popup.hide();
-  $('fbPopupNib').click(function() { popup.toggle(); });
+  FB.init({
+    appId      : '288106721255039',
+    status     : true,
+    cookie     : true,
+    xfbml      : true,
+    oauth      : true,
+  });
 
-  load_fbjs();
+  FB.Event.subscribe('auth.statusChange', setStatusOnResponse);
+
+  var popup = $('.fbPopup');
+  popup.hide();
+  $('.fbPopupNib').click(function() { popup.toggle(); })
+    .attr('src', chrome.extension.getURL('nib-icon.png'));
+
+  $('.arrow').click(function() {
+    var button = $(this);
+    if (button.parent().hasClass('likes')) {
+      upvote(button);
+    } else if (button.parent().hasClass('dislikes')) {
+      // downvote();
+    } else {
+      if (button.hasClass('up')) {
+        unupvote(button);
+      } else {
+        // undownvote();
+      }
+    }
+  });
 });
